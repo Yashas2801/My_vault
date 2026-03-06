d0b768ff2750e981516ec696a656b3d2811735c4
Yashas2801 <yashashs99999@gmail.com>
2026-03-06 17:08:11 +0530

    Correct baseUrl with subfolder for GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "Yashas2801.github.io",
+    baseUrl: "Yashas2801.github.io/My_vault",
     ignorePatterns: ["private", "templates", "Template", ".obsidian", ".trash", ".stfolder", ".stfolder.removed-*", "Untitled*.base"],
     defaultDateType: "modified",
     theme: {
